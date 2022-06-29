acdf565c7a2a6b2b2f19010dbad3c001f1712f67
Tuukka <hartikainentuukka@gmail.com>
2022-06-29 14:30:33 +0300

    päivityksiä

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
+const mongoose = require('mongoose')
 
 let persons = [
     {

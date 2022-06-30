d667e48663d7d328dd1158024af23fbaf4fc3090
Tuukka <hartikainentuukka@gmail.com>
2022-06-30 09:03:44 +0300

    toimii tietokannan kanssa

--- index.js
+++ index.js
@@ -39,16 +39,17 @@ app.get('/api/info', (request, response) => {
   response.send()
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    
-    if (person) {
-      response.json(person)
-    } else {
-      response.status(404).end()
-    }
-  })
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+      .then(person =>{
+        if (person) {
+          response.json(person)
+        } else {
+          response.status(404).end()
+        }
+      })
+      .catch(error => next(error))
+})
 
 app.post('/api/persons', (request, response)=>{
     const body = request.body
@@ -81,12 +82,33 @@ app.post('/api/persons', (request, response)=>{
 
 })
 
-app.delete('/api/persons/:id', (request, response)=>{
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
+app.delete('/api/persons/:id', (request, response, next)=>{
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 
-  response.status(204).end()
+  
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {

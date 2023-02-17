9dbae627b23cce8189985826e3cceae63a1678c7
Tuukka Hartikainen <hartikainentuukka@gmail.com>
2023-02-17 11:20:53 +0100

    added new ui

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.get('/api/persons/:id', (request, response, next) => {
       .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response)=>{
+app.post('/api/persons', (request, response, next)=>{
     const body = request.body
 
     if (body.name === undefined) {
@@ -79,6 +79,7 @@ app.post('/api/persons', (request, response)=>{
     person.save().then(savedPerson =>{
       response.json(savedPerson)
     })
+    .catch(error => next(error))
 
 })
 
@@ -87,7 +88,10 @@ app.delete('/api/persons/:id', (request, response, next)=>{
     .then(result => {
       response.status(204).end()
     })
-    .catch(error => next(error))
+    .catch(error => {
+      
+      next(error)
+    })
 
   
 })
@@ -103,8 +107,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
-
   next(error)
 }
 
